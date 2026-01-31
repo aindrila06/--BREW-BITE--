d3c02d064e01ff7decc844604d78a43535199e12
aindrila06 <154113395+aindrila06@users.noreply.github.com>
2026-01-31 06:22:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'a_super_secret_key_for_your
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafe.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 
 
 # --- Flask-Mail Configuration ---
@@ -665,4 +668,5 @@ def get_kitchen_notifications():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+
     app.run(debug=True)
